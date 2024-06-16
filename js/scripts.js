be494f29d570a9d262fba17960588cb1f537b005
Ralph Pastel <ralph.pastel@gmail.com>
2024-06-16 22:32:16 +0200

    Created pokemonList variable
    I added multiple objects in that variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-let favoriteFood = "rice";
-console.log(favoriteFood);
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Bulbasaur", type: ["Monster", "Grass"], height: 0.7, weight: 6.9 },
+  { name: "Ivysaur", type: ["Monster", "Grass"], height: 1, weight: 13 },
+  { name: "Venusaur", type: ["Monster", "Grass"], height: 2, weight: 100 },
+  { name: "Charmander", type: ["Monster", "Dragon"], height: 2, weight: 100 },
+];
