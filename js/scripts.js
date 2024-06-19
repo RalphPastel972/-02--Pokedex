0cfc03ec91abbf85f58c26767555562ff6338151
Ralph Pastel <ralph.pastel@gmail.com>
2024-06-19 15:54:26 +0200

    Replaced For loop by forEach
    And I used IIFE as well

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
-let pokemonList = [
-  { name: "Bulbasaur", type: ["Monster", "Grass"], height: 0.7, weight: 6.9 },
-  { name: "Ivysaur", type: ["Monster", "Grass"], height: 1, weight: 13 },
-  { name: "Venusaur", type: ["Monster", "Grass"], height: 2, weight: 100 },
-  { name: "Charmander", type: ["Monster", "Dragon"], height: 2, weight: 100 },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", type: ["Monster", "Grass"], height: 0.7, weight: 6.9 },
+    { name: "Ivysaur", type: ["Monster", "Grass"], height: 1, weight: 13 },
+    { name: "Venusaur", type: ["Monster", "Grass"], height: 2, weight: 100 },
+    { name: "Charmander", type: ["Monster", "Dragon"], height: 2, weight: 100 },
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 1) {
-    document.write(
-      `${pokemonList[i].name} has a height of ${pokemonList[i].height}m - Waw! That's small !<br>`
-    );
-    // I chose the smallest Pokemon instead of the biggest, I hope that is Ok! :-)
-  } else {
-    document.write(
-      `${pokemonList[i].name} has a height of ${pokemonList[i].height}m <br>`
-    );
-  }
-}
+
+  pokemonList.forEach((pokemonPassport) => {
+    console.log(pokemonPassport);
+
+    if (pokemonPassport.height < 1) {
+      document.write(
+        `${pokemonPassport.name} has a height of ${pokemonPassport.height}m - Waw! That's small !<br>`
+      );
+    } else {
+      document.write(
+        `${pokemonPassport.name} has a height of ${pokemonPassport.height}m<br>`
+      );
+    }
+  });
+
+})();
